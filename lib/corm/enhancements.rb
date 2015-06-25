8bc8e061353d61b58aa45f4e3bafd1ba589b1e78
Edoardo Rossi <edd.rossi@gmail.com>
2015-06-25 11:43:36 +0200

    Add some entropy in the retry time.
    The result is something like this:
    
    ```
    WARN -- : Host 127.0.0.1 refused all connections
    ERROR -- : Session failed to connect (Cassandra::Errors::NoHostsAvailable: All hosts down)
    ERROR -- : (1/3 attempts) Bad fail! Retry in 2.0 seconds to recover  Cassandra::Errors::NoHostsAvailable: All hosts down
    ERROR -- : Session failed to connect (Cassandra::Errors::NoHostsAvailable: All hosts down)
    ERROR -- : (2/3 attempts) Bad fail! Retry in 4.24 seconds to recover  Cassandra::Errors::NoHostsAvailable: All hosts down
    ERROR -- : Control connection failed (Cassandra::Errors::NoHostsAvailable: All hosts down)
    ERROR -- : (3/3 attempts) Bad fail! Retry in 6.18 seconds to recover  Cassandra::Errors::NoHostsAvailable: All attempted hosts failed: 127.0.0.1 (Cassandra::Errors::IOError: Connection refused - connect(2))
    ```

--- lib/corm/enhancements.rb
+++ lib/corm/enhancements.rb
@@ -43,8 +43,9 @@ module Corm
         begin
           return block.call() if block_given?
         rescue *RESCUED_CASSANDRA_EXCEPTIONS => e
-          enhancements_logger.error { "(#{i}/#{attempts} attempts) Bad fail! Retry in #{i*2} seconds to recover  #{e.class.name}: #{e.message}" }
-          sleep(i*2)
+          sleep_for = i * Integer((rand+1.5) * 100) / Float(100)
+          enhancements_logger.error { "(#{i}/#{attempts} attempts) Bad fail! Retry in #{sleep_for} seconds to recover  #{e.class.name}: #{e.message}" }
+          sleep(sleep_for)
         end
       end
       nil
